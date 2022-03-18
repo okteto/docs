e34a962145d5101c98e34344a2ad41181c13b689
Hugues Tennier <hugues@okteto.com>
2022-03-18 13:12:55 -0400

    Fix errors

--- src/theme/DocPaginator/index.js
+++ src/theme/DocPaginator/index.js
@@ -8,7 +8,7 @@ export default function DocPaginatorWrapper(props) {
   return (
     <>
       <BrowserOnly>
-        <Feedback />
+        {() => <Feedback />}
       </BrowserOnly>
       <DocPaginator {...props} />
     </>
