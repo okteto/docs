e75edb13ca7278aea6afc760924ee5e8a7dd299c
Hugues Tennier <hugues@okteto.com>
2022-03-18 12:20:02 -0400

    Handle session storage

--- src/components/Feedback/index.js
+++ src/components/Feedback/index.js
@@ -6,21 +6,32 @@ import ThumbsDownIcon from "../../icons/ThumbsDownIcon";
 import Button from "../../theme/Button";
 import styles from './styles.module.scss';
 
+const SESSION_STORAGE_KEY = "OktetoDocsFeedback";
+
 const Feedback = () => {
   const [expanded, setExpanded] = useState(false);
   const [pageTitle, setPageTitle] = useState("");
-  const [submitted, setSubmitted] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [feedback, setFeedback] = useState ({submitted: false, helpful: ""})
+  const [gaveFeedbackForThesePagesURLs, setGaveFeedbackForThesePagesURLs] = useState(sessionStorage.getItem(SESSION_STORAGE_KEY) || false);
 
   const form = useRef(null)
 
   useEffect(() => {
-    setPageTitle(document.title.replace(" | Okteto Documentation", ""))
+    setPageTitle(document.title.replace(" | Okteto Documentation", ""));
+
+    if(gaveFeedbackForThesePagesURLs) {
+      const pages = JSON.parse(gaveFeedbackForThesePagesURLs);
+      const currentPageFeedback = pages.find(page => page.url === window.location.href);
+      if(currentPageFeedback) {
+        setFeedback({submitted: true, helpful: currentPageFeedback.helpful})
+      }
+    }
   }, [])
 
   const submit = e => {
     e.preventDefault();
-
+    
     setLoading(true);
 
     const data = {
@@ -31,35 +42,44 @@ const Feedback = () => {
       submittedOn: new Date().toLocaleDateString('en-CA')
     }
 
+    handleSessionStorage({url: data.pageURL, helpful: data.helpful});
+
     fetch('/.netlify/functions/gather-feedback', { method: 'POST', body: JSON.stringify(data) }).then(response => {
-      if(response.status === 200) setSubmitted(true)
-      // setLoading(false);
+      if(response.status === 200) setFeedback({submitted: true})
+      setLoading(false);
     })
   }
 
+  const handleSessionStorage = ({url, helpful}) => {
+    let pageURLs = JSON.parse(gaveFeedbackForThesePagesURLs) || [];
+    pageURLs.push({url: url, helpful});
+    sessionStorage.setItem(SESSION_STORAGE_KEY, JSON.stringify(pageURLs));
+  }
+
   const handleRadioChange = () => {
+    setFeedback({helpful: form.current.helpful.value})
     setExpanded(true);
   }
 
   return (
     <>
       <h2 className={styles.componentTitle}>Was this page helpful?</h2>
-      <form ref={form} onSubmit={submit} className={`${styles.form} ${expanded ? styles.formExpanded : ''}`}>
+      <form ref={form} onSubmit={submit} className={`${styles.form} ${expanded || feedback.submitted ? styles.formExpanded : ''}`}>
         <div className={styles.radioContainer}>
           <div className={styles.radio}>
-            <input type="radio" name="helpful" value="yes" className={`${styles.radioInput} ${styles.radioYes}`} id="feedback-helpful-yes" onChange={handleRadioChange} />
+            <input type="radio" name="helpful" value="yes" checked={feedback.helpful === "yes"} className={`${styles.radioInput} ${styles.radioYes}`} id="feedback-helpful-yes" onChange={handleRadioChange} />
             <label className={styles.radioLabel} htmlFor="feedback-helpful-yes">
               <ThumbsUpIcon />
             </label>
           </div>
           <div className={styles.radio}>
-            <input type="radio" name="helpful" value="no" className={`${styles.radioInput} ${styles.radioNo}`} id="feedback-helpful-no" onChange={handleRadioChange} />
+            <input type="radio" name="helpful" value="no" checked={feedback.helpful === "no"} className={`${styles.radioInput} ${styles.radioNo}`} id="feedback-helpful-no" onChange={handleRadioChange} />
             <label className={styles.radioLabel} htmlFor="feedback-helpful-no">
               <ThumbsDownIcon />
             </label>
           </div>
         </div>
-        {!submitted &&
+        {!feedback.submitted &&
           <>
             <textarea name="feedback" className={styles.comment} placeholder="Place add your feedback (optional)" hidden={!expanded}></textarea>
             <Button type="submit" className={styles.submitButton} hidden={!expanded} role="button" loading={loading}>
@@ -67,7 +87,7 @@ const Feedback = () => {
             </Button>
           </>
         }
-        {submitted &&
+        {feedback.submitted &&
           <p className={styles.feedbackTitle}>Thanks for the feedback!</p>
         }
       </form>
