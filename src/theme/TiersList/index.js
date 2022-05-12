e878ebb138d7fe596f48d50cd1ea44808671b138
Hugues Tennier <hugues@okteto.com>
2022-05-12 11:55:43 -0400

    Add prop types

--- src/theme/TiersList/index.js
+++ src/theme/TiersList/index.js
@@ -1,19 +1,25 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 import "./styles.scss";
 
-function TiersList({tiers}) {
+const TiersList = ({tiers}) => {
+
+  if(typeof tiers !== 'string') return null;
 
   const tiersArray = tiers.split(" ");
 
-  console.log(tiersArray);
   return (
     <div className="TiersList">
-    {tiersArray.map(tier => (
-      <span className="Tier">{tier}</span>
-    ))}
+      {tiersArray.length > 0 && tiersArray.map((tier, index) => (
+        <span className="Tier" key={`${tier}-${index}`}>{tier}</span>
+      ))}
     </div>
   );
 }
+
+TiersList.propTypes = {
+  tiers: PropTypes.string.isRequired
+};
 
 export default TiersList;
