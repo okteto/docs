1fdc2dec647a8214eb6d241ff113d3fcfbaeb634
Pablo Chico de Guzman <pchico83@gmail.com>
2022-10-10 12:42:30 +0200

    A general pass to the volume snapshots feature

--- sidebars.js
+++ sidebars.js
@@ -42,6 +42,7 @@ module.exports = {
       },
       "cloud/build",
       "cloud/registry",
+      "cloud/volume-snapshots",
       "cloud/namespaces",
       "cloud/credentials",
       "cloud/personal-access-tokens",
@@ -105,7 +106,7 @@ module.exports = {
           "self-hosted/administration/custom-installer-image",
           "self-hosted/administration/configuration",
           "self-hosted/administration/github",
-          "self-hosted/administration/volume-snapshots"
+          "self-hosted/administration/enable-volume-snapshots"
         ]
       }
     ],
