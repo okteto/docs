82d11dcc475d5105f6821af21645bd2e4f8e3694
Ramiro Berrelleza <ramiro@okteto.com>
2023-01-27 18:11:12 -0800

    only have current and next in docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -155,31 +155,6 @@ module.exports = {
           versions: {
             current: {
               label: '1.5 (not yet released)',
-            },
-            '1.4': {
-              label: '1.4',
-              path: '1.4',
-              banner: 'none',
-            },
-            '1.3': {
-              label: '1.3',
-              path: '1.3',
-              banner: 'none',
-            },
-            '1.2': {
-              label: '1.2',
-              path: '1.2',
-              banner: 'none',
-            },
-            '1.1': {
-              label: '1.1',
-              path: '1.1',
-              banner: 'none',
-            },
-            '1.0': {
-              label: '1.0',
-              path: '1.0',
-              banner: 'none',
             }
           },
           include: [
