33d5172aa2ef96b82a2df6ed8b570131be992d22
Francisco Miranda <franciscomirandaa@gmail.com>
2023-09-28 18:35:16 -0300

    add 1.13 versionsed docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -154,9 +154,13 @@ module.exports = {
           editUrl: 'https://github.com/okteto/docs/edit/main',
           breadcrumbs: false,
           sidebarPath: require.resolve('./sidebars.js'),
-          lastVersion: '1.12',
+          lastVersion: '1.13',
           versions: {
             current: {
+              label: '1.14',
+              path: '1.14',
+            },
+            '1.13': {
               label: '1.13',
               path: '1.13',
             },
