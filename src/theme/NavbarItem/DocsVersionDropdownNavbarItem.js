c1b75fe06a8e34c25c5857f7a141f0df3ce5c359
Ramon Lamana <rlamana@gmail.com>
2024-02-13 18:18:33 +0100

    Show last 8 versions in the versions menu

--- src/theme/NavbarItem/DocsVersionDropdownNavbarItem.js
+++ src/theme/NavbarItem/DocsVersionDropdownNavbarItem.js
@@ -12,11 +12,10 @@ import {
   useLatestVersion,
   useActiveDocContext,
 } from '@docusaurus/plugin-content-docs/client';
-import {useDocsPreferredVersion} from '@docusaurus/theme-common';
-import {translate} from '@docusaurus/Translate';
+import { useDocsPreferredVersion } from '@docusaurus/theme-common';
+import { translate } from '@docusaurus/Translate';
 
-const getVersionMainDoc = (version) =>
-  version.docs.find((doc) => doc.id === version.mainDocId);
+const getVersionMainDoc = (version) => version.docs.find((doc) => doc.id === version.mainDocId);
 
 export default function DocsVersionDropdownNavbarItem({
   mobile,
@@ -29,16 +28,14 @@ export default function DocsVersionDropdownNavbarItem({
   const activeDocContext = useActiveDocContext(docsPluginId);
   const versions = useVersions(docsPluginId);
   const latestVersion = useLatestVersion(docsPluginId);
-  const {preferredVersion, savePreferredVersionName} =
-    useDocsPreferredVersion(docsPluginId);
+  const { preferredVersion, savePreferredVersionName } = useDocsPreferredVersion(docsPluginId);
 
   function getItems() {
     const versionLinks = versions.map((version) => {
       // We try to link to the same doc, in another version
       // When not possible, fallback to the "main doc" of the version
       const versionDoc =
-        activeDocContext?.alternateDocVersions[version.name] ||
-        getVersionMainDoc(version);
+        activeDocContext?.alternateDocVersions[version.name] || getVersionMainDoc(version);
       return {
         isNavLink: true,
         label: version.label,
@@ -52,23 +49,39 @@ export default function DocsVersionDropdownNavbarItem({
     return [...dropdownItemsBefore, ...versionLinks, ...dropdownItemsAfter];
   }
 
-  const items = getItems();
-  const dropdownVersion =
-    activeDocContext.activeVersion ?? preferredVersion ?? latestVersion; // Mobile dropdown is handled a bit differently
+  function compareLinkVersions(a, b) {
+    const partsA = a.label?.split?.('.').map(Number) ?? 0;
+    const partsB = b.label?.split?.('.').map(Number) ?? 0;
+    for (let i = 0; i < Math.max(partsA.length, partsB.length); i++) {
+      const numA = partsA[i] || 0;
+      const numB = partsB[i] || 0;
+
+      if (numA < numB) {
+        return 1; // Reverse the order for descending
+      } else if (numA > numB) {
+        return -1; // Reverse the order for descending
+      }
+    }
+    return 0;
+  }
+
+  let items = getItems();
+  const navLinks = items.filter((item) => !!item.isNavLink);
+  const restLinks = items.filter((item) => !item.isNavLink);
+  items = [...navLinks.sort(compareLinkVersions).slice(0, 8), ...restLinks];
+
+  const dropdownVersion = activeDocContext.activeVersion ?? preferredVersion ?? latestVersion; // Mobile dropdown is handled a bit differently
 
   const dropdownLabel =
     mobile && items.length > 1
       ? translate({
           id: 'theme.navbar.mobileVersionsDropdown.label',
           message: 'Versions',
-          description:
-            'The label for the navbar versions dropdown on mobile view',
+          description: 'The label for the navbar versions dropdown on mobile view',
         })
       : dropdownVersion.label;
   const dropdownTo =
-    mobile && items.length > 1
-      ? undefined
-      : getVersionMainDoc(dropdownVersion).path; // We don't want to render a version dropdown with 0 or 1 item. If we build
+    mobile && items.length > 1 ? undefined : getVersionMainDoc(dropdownVersion).path; // We don't want to render a version dropdown with 0 or 1 item. If we build
   // the site with a single docs version (onlyIncludeVersions: ['1.0.0']),
   // We'd rather render a button instead of a dropdown
 
