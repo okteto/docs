aadf34b6e9f4d063208c63494a3deecc3180d48e
Pablo Chico de Guzman <pchico83@gmail.com>
2024-10-22 07:45:42 +0200

    Don't host chart dependencies on the "okteto" DockerHub organization

--- sidebars.js
+++ sidebars.js
@@ -245,7 +245,7 @@ module.exports = {
             'self-hosted/manage/buildkit-high-performance',
             'self-hosted/manage/backup',
             'self-hosted/manage/custom-resource-definitions',
-            'self-hosted/manage/airgap',
+            'self-hosted/manage/air-gapped',
             'self-hosted/manage/troubleshooting',
             'self-hosted/manage/diagnostics',
             'self-hosted/manage/uninstall-okteto',
