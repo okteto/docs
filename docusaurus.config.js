79b4393c496ee86f67d30d5a976fd26b7e2532d8
Cody J Landstrom <cody.landstrom@gmail.com>
2024-11-11 07:51:17 -0800

    Create 1.26 docs release (#913)
    Co-authored-by: Cody Landstrom <cody@okteto.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -239,20 +239,25 @@ module.exports = {
           editUrl: 'https://github.com/okteto/docs/edit/main',
           breadcrumbs: false,
           sidebarPath: require.resolve('./sidebars.js'),
-          lastVersion: '1.25',
+          lastVersion: '1.26',
           versions: {
             current: {
               // aka unreleased version in development
               // Remember to also update "unreleased" redirect if changing the value!
-              label: '1.26',
-              path: '1.26',
+              label: '1.27',
+              path: '1.27',
             },
-            1.25: {
+            1.26: {
               // aka latest/official version
               // Remember to also update docs root redirect if changing the value!
-              label: '1.25',
+              label: '1.26',
               path: '/',
               banner: 'none',
+            },
+            1.25: {
+              label: '1.25',
+              path: '1.25',
+              banner: 'unmaintained',
             },
             1.24: {
               label: '1.24',
@@ -273,12 +278,7 @@ module.exports = {
               label: '1.21',
               path: '1.21',
               banner: 'unmaintained',
-            },
-            '1.20': {
-              label: '1.20',
-              path: '1.20',
-              banner: 'unmaintained',
-            },
+            }
           },
           include: ['**/*.md', '**/*.mdx'],
         },
