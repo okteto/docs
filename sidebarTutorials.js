42a2152b1b1ba9beb7dda5ab41a00ee52f653926
Hugues Tennier <hugues@okteto.com>
2025-12-04 23:48:19 -0500

    Tutorial: How to Create a Development Environment with Realistic Data in Okteto (#959)
    * Add tutorial on how to use volume snapshots
    
    Co-authored-by: Cody Landstrom <cody@okteto.com>
    Co-authored-by: Ramiro <ramiro@okteto.com>

--- sidebarTutorials.js
+++ sidebarTutorials.js
@@ -30,6 +30,7 @@ module.exports = {
         'using-launchdarkly-and-okteto-to-automate-modern-feature-flag-management',
         'divert',
         'optimize-your-development-environment',
+        'create-and-use-volume-snapshots',
       ],
     },
     {
