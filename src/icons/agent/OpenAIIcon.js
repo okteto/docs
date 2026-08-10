215c29ff2c06a87751963eb68dddc24d61759923
Cody Landstrom <cody@okteto.com>
2026-08-09 22:48:03 -0700

    feat(theme): add agent/markdown icon components
    Signed-off-by: Cody Landstrom <cody@okteto.com>

---
+++ src/icons/agent/OpenAIIcon.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const OpenAIIcon = (props) => (
+  <svg viewBox="0 0 24 24" width="16" height="16" fill="currentColor" aria-hidden="true" {...props}>
+    <path d="M12 4c-4.97 0-9 3.13-9 7 0 2.22 1.33 4.2 3.4 5.48-.12.9-.53 2.06-1.4 3.02 1.85-.14 3.32-.86 4.28-1.55.86.22 1.77.34 2.72.34 4.97 0 9-3.13 9-7s-4.03-7-9-7z" />
+  </svg>
+);
+
+export default OpenAIIcon;
